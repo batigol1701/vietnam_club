1483e451008d0bb63c3eeb408f0760a381e66ba2
chacharoti <chacharoti@gmail.com>
2017-06-12 05:21:12 -0700

    Update vfsc page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
   def show
     @category = get_category
-    @home_events = Content.find_by slug: 'home_event' if @category.title == 'Vietnam Bussiness Club'
+    @home_events = (Content.find_by slug: 'home_event') if @category.title == 'Vietnam Bussiness Club'
+    @vfsc_contents = (Content.where slug: 'vfsc_contents') if @category.title == 'Vietnam Foreign Student Club'
   end
 
   private
