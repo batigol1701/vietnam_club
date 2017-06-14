833690708f681e0913c62777a89b3820c334ce77
chacharoti <chacharoti@gmail.com>
2017-06-14 08:39:38 -0700

    register as member

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :skip_password_validation
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  protected
+
+  def password_required?
+    return false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612064900) do
+ActiveRecord::Schema.define(version: 20170614151302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,22 @@ ActiveRecord::Schema.define(version: 20170612064900) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.date     "register_date"
+    t.boolean  "invoice"
+    t.string   "enterprise_name"
+    t.string   "address"
+    t.string   "full_name"
+    t.string   "position"
+    t.string   "department"
+    t.string   "tel"
+    t.string   "fax"
+    t.string   "url"
+    t.string   "other"
+    t.string   "gender"
+    t.date     "birthday"
+    t.string   "school_name"
+    t.string   "education"
+    t.string   "type"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
